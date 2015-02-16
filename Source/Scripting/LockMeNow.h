3b5aba17e1bd3fc4fe2b17d9be9c76a05537fd55
Vitalii Parovishnyk <Korich@Korichs-MacBook-Pro.local>
2015-02-16 22:54:36 +0200

    Added a test scripting for app

---
+++ Source/Scripting/LockMeNow.h
@@ -0,0 +1,15 @@
+//
+//  ScriptApplication.h
+//  LockMeNow
+//
+//  Created by Vitalii Parovishnyk on 2/16/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSApplication (LockMeNow)
+
+- (NSNumber*)takePhoto;
+
+@end
